b97e58b41a6ead403cb73462264b21325d8c29ad
Samuel Kojicovski <kojicovski@gmail.com>
2020-09-16 20:40:12 -0300

    exercise 01-02

---
+++ test01.py
@@ -0,0 +1,16 @@
+nome = input('Qual é o seu nome?')
+print('Olá',nome,'! Prazer em te conhecer!')
+
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('ano = ')
+
+print(nome,'você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
+
+num1 = int(input('1° Número'))
+num2 = int(input('2° Número'))
+
+soma = num1 + num2
+
+print('A soma dos numerods digitados é:',soma)
+
