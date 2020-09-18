191007738a9decdb74325c2474247d2c85b00227
Samuel Kojicovski <kojicovski@gmail.com>
2020-09-17 21:49:24 -0300

    class 07

---
+++ course/class07a.py
@@ -0,0 +1,10 @@
+n1 = int(input('Value one: '))
+n2 = int(input('Value two: '))
+
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print(f'The sum is {s}, multiplication is {s}, division is {d:.3f}, entire divison is {di}, potentiation is {e} ')
