e64501c4548993102b571f2e383b836855a21dc9
Samuel Kojicovski <kojicovski@gmail.com>
2020-09-20 15:37:24 -0300

    fixed txt on the file

--- exercises/ex018.py
+++ exercises/ex018.py
@@ -1,5 +1,5 @@
 from math import sin, cos
 
-degree = int(input('Type a value in degree (Cº) :'))
+degree = int(input('Type a value in degree :'))
 
 print('Value of sin {:.4} and cos {:.4} of {} degree'.format(sin(degree),cos(degree),degree))
