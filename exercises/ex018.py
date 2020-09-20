b1b313c5c9c14b77e04c29627c28f5091dab27ca
Samuel Kojicovski <kojicovski@gmail.com>
2020-09-20 19:20:55 -0300

    fixed and convert radians to degree

--- exercises/ex018.py
+++ exercises/ex018.py
@@ -1,5 +1,6 @@
-from math import sin, cos
+from math import sin, cos, radians, tan
 
 degree = int(input('Type a value in degree :'))
 
-print('Value of sin {:.4} and cos {:.4} of {} degree'.format(sin(degree),cos(degree),degree))
+print('Value of sin {:.4f}, cos {:.4f} and tg {:.4f} of {} degree'.format(sin(radians(degree)), \
+    cos(radians(degree)), tan(radians(degree)), degree))
