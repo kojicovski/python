c8d3cdba9c578a85d9069e847b0a4bb46ea4b76d
Samuel Kojicovski <kojicovski@gmail.com>
2020-09-23 21:32:47 -0300

    Added two features

--- exercises/ex024.py
+++ exercises/ex024.py
@@ -1,6 +1,8 @@
-city = input('Type the name of your City')
+city = input('Type the name of your City').strip()
+city = city.upper()
 
-if city.count('Santo', 0, 5):
+
+if city.count('SANTO', 0, 5):
     print('Sua cidade começa com Santo')
 else:
     print('Sua cidade não começa com Santo')
