d271016f4849037f2160bd8470cb5f4b2185cbc8
Samuel Kojicovski <kojicovski@gmail.com>
2020-09-23 21:09:24 -0300

    Added new method to separate numbers

--- exercises/ex023.py
+++ exercises/ex023.py
@@ -1,35 +1,35 @@
-from math import trunc
-
 num = input('Type a number from 0 to 9999: ')
 num1 = int(input('Type another number from 0 to 9999'))
 
 if len(num) == 4:
-    print(f'Unidade: {num[3]}')
-    print(f'Dezena: {num[2]}')
-    print(f'Centena: {num[1]}')
-    print(f'Milhar: {num[0]}')
+    print(f'Unit: {num[3]}')
+    print(f'Ten: {num[2]}')
+    print(f'Hundred: {num[1]}')
+    print(f'Thousands: {num[0]}')
 elif len(num) == 3:
-    print(f'Unidade: {num[2]}')
-    print(f'Dezena: {num[1]}')
-    print(f'Centena {num[0]}')
-    print(f'Milhar: 0')
+    print(f'Unit: {num[2]}')
+    print(f'Ten: {num[1]}')
+    print(f'Hundred {num[0]}')
+    print(f'Thousands: 0')
 elif len(num) == 2:
-    print(f'Unidade: {num[1]}')
-    print(f'Dezena: {num[0]}')
-    print(f'Centena: 0')
-    print(f'Milhar: 0')
+    print(f'Unit: {num[1]}')
+    print(f'Ten: {num[0]}')
+    print(f'Hundred: 0')
+    print(f'Thousands: 0')
 elif len(num) == 1:
-    print(f'Unidade: {num}')
-    print(f'Dezena: 0')
-    print(f'Centena: 0')
-    print(f'Milhar: 0')
+    print(f'Unit: {num}')
+    print(f'Ten: 0')
+    print(f'Hundred: 0')
+    print(f'Thousands: 0')
 
-if num1 > 999:
-    aux1 = num1 / 1000
-    print(f'Milhar{trunc(aux1)}')
+#Doing with math
 
-    aux2 = num1 / 100
-    aux2 = aux2 // 10
-    print(f'Dezena')
+u = (num1 // 1 % 10)
+d = (num1 // 10 % 10)
+c = (num1 // 100 % 10)
+m = (num1 // 1000 % 10)
 
-#Need a feature
+print(f'Unit: {u}')
+print(f'Ten: {d}')
+print(f'Hundred: {c}')
+print(f'Thousands: {m}')
