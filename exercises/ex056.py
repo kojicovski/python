dc7ae7f5ede231b9f52f8c83cde7e8e1a7733140
Samuel Kojicovski <kojicovski@gmail.com>
2020-09-29 23:23:38 -0300

    Exercise 056 - Not finished

---
+++ exercises/ex056.py
@@ -0,0 +1,34 @@
+class People:
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+
+    def foo(self):
+        #return f'Your weight is {self.weight}Kg, to have {self.age} and you are {self.sex}'
+        return self.name, self.age, self.sex
+
+
+p = []
+
+for i in range(2):
+    name = str(input(f'{i}-Whats your name: '))
+    age = int(input(f'{i}-Type your age: '))
+    sex = str(input(f'{i}-Type your sex: ')).strip()
+    p.append(People(name, age, sex))
+    print(p[i].foo())
+
+aveg_age = 0.0
+old_guy = ''
+age_old_guy = 0
+
+for i in range(2):
+    aveg_age = (aveg_age + p[i].age)
+    if i == 4:
+        aveg_age = aveg_age / i
+    if p[i].age > age_old_guy:
+        age_old_guy = p[i].age
+        old_guy = p[i].name
+
+print(f'Average age {aveg_age}')
+print(f'Old guy is {old_guy}')
